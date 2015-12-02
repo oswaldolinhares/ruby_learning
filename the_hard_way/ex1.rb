8cef775c2b041a48d3e5903775aae384a1973c27
Oswaldo Linhares <oswaldolinhares@gmail.com>
2015-12-02 09:21:36 -0300

    Exercícios do site "Learn Ruby - The Hard Way"

---
+++ the_hard_way/ex1.rb
@@ -0,0 +1,6 @@
+puts "Hello World"
+puts "Hello Again"
+puts "I like typing this."
+puts "Yay! Priting"
+puts "I'd must rather you 'not'."
+puts 'I "said"do not touch this.'
---
+++ the_hard_way/ex2.rb
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Isto é um comentário
+# Em Ruby o que for digitado após o # é ignorado pelo interpretador
+puts "I could have code like this" # comentários após o fechamento da aspas é ignorado
+
+puts "This will run."
+
